570d5fe5696697f44fbeef50eb3934d88a256dea
ralston0220 <b8047585@gmail.com>
2024-03-14 14:42:01 +0200

    changed variable name

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
+    
+
+
     let result = ''; // Placeholder for validation result
+
+    let  validInput = /^pet_[a-zA-Z0-9]+$/;
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
-
+        
+        if (validInput.test(input)) { 
+            result = 'Valid Syntax'
+        } else {
+            result = 'Invalid Syntax'
+        }
+     
+        
             document.getElementById('result').innerText = result;
 }
 
