af7f13958ce10424839c40ca400eabf4407189d4
Ralston Berry <b8047585@gmail.com>
2024-03-15 08:25:57 +0200

    fixed the layout of the code

--- script.js
+++ script.js
@@ -11,14 +11,14 @@ function validateSyntax() {
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
         
-        if (validInput.test(input)) { 
+     if (validInput.test(input)) { 
             result = 'Valid Syntax'
         } else {
             result = 'Invalid Syntax'
         }
      
         
-            document.getElementById('result').innerText = result;
+     document.getElementById('result').innerText = result;
 }
 
 
